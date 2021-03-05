cf281bdacc52dc92bd821f615d7f8fabb7e0c074
孙龙飞 <oncwnuEjFiUdXz6A-ziqo-0ciDCM@git.weixin.qq.com>
2021-03-05 13:04:21 +0800

    获取表单内容

--- lib/pages/TextField.dart
+++ lib/pages/TextField.dart
@@ -8,6 +8,13 @@ class TextFieldDemoPage extends StatefulWidget {
 }
 
 class _TextFieldDemoPageState extends State<TextFieldDemoPage> {
+  var _username = new TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _username.text = '初始值';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,44 +27,22 @@ class _TextFieldDemoPageState extends State<TextFieldDemoPage> {
               padding: EdgeInsets.all(20),
               child: Column(
                 children: <Widget>[
-                  TextField(), // 默认文本框
-                  SizedBox(
-                    height: 20,
-                  ),
                   TextField(
-                    decoration: InputDecoration(
-                        hintText: '有边框的文本框', border: OutlineInputBorder()),
+                    decoration: InputDecoration(hintText: '请输入用户名'),
+                    controller: _username,
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    maxLines: 4,
-                    decoration: InputDecoration(
-                        hintText: '多行文本框', border: OutlineInputBorder()),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                        hintText: '请输入用户名',
-                        labelText: '用户名',
-                        icon: Icon(Icons.people),
-                        border: OutlineInputBorder()),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: '请输入密码框',
-                        labelText: '密码',
-                        icon: Icon(Icons.pages),
-                        // labelStyle: TextStyle(),
-                        border: OutlineInputBorder()),
-                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    child: RaisedButton(
+                        child: Text('登录'),
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          print(_username.text);
+                        }),
+                  )
                 ],
               ),
             )
